af117436c3bf8b043a38be57b96240efc5f4c353
rjs <ramonjsantiago@gmail.com>
2025-11-18 11:41:09 -0500

    Chunk 6

--- src/main/java/com/fileexplorer/MainApp.java
+++ src/main/java/com/fileexplorer/MainApp.java
@@ -6,13 +6,33 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
+    private Scene scene;
+
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/fileexplorer/ui/MainLayout.fxml"));
-        Scene scene = new Scene(loader.load(), 1200, 800);
+        scene = new Scene(loader.load(), 1200, 800);
+
+        // Default theme: light
+        scene.getStylesheets().add(getClass().getResource("/com/fileexplorer/ui/css/light.css").toExternalForm());
+
         stage.setTitle("Ultra File Explorer");
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void setTheme(String theme) {
+        scene.getStylesheets().clear();
+        switch (theme.toLowerCase()) {
+            case "dark":
+                scene.getStylesheets().add(getClass().getResource("/com/fileexplorer/ui/css/dark.css").toExternalForm());
+                break;
+            case "glassy":
+                scene.getStylesheets().add(getClass().getResource("/com/fileexplorer/ui/css/glassy.css").toExternalForm());
+                break;
+            default:
+                scene.getStylesheets().add(getClass().getResource("/com/fileexplorer/ui/css/light.css").toExternalForm());
+        }
     }
 
     public static void main(String[] args) {
